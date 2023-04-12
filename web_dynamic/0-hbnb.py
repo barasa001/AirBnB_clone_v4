3af05b7accbab0c3fb37179d3d5f8124f36bb466
barasa001 <s.wanyonyi@alustudent.com>
2023-04-11 22:43:46 -0700

    commit

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -18,7 +18,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('0-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -33,11 +33,13 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    cache_id = (str(uuid.uuid4()))
 
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
